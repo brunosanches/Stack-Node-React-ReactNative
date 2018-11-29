e9a7b95c01124b1589fcf624ea1f7073e9b281e2
brunosanches <obrunosanches@gmail.com>
2018-11-28 23:35:19 -0200

    Init bcrypt password

--- go-node/modulo2/src/app/models/User.js
+++ go-node/modulo2/src/app/models/User.js
@@ -1,8 +1,11 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
     password_hash: DataTypes.STRING,
     provider: DataTypes.BOOLEAN
   })
